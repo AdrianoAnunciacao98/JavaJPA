f38ce51fbde6c4f63b251fcb06a7d8c64bc4b946
= <=>
2021-07-16 14:33:59 -0300

    Estados Managed, Transient e Removed

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -14,7 +14,18 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private String titular;
+	private Double saldo;
 	
+	
+	
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -47,6 +58,7 @@ public class Conta {
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
+
 	
 	
 
